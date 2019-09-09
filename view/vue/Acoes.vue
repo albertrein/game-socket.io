8dbf2e45ca2ddf4057721a10df1a284adac09c67
guilhermear <guilhermear@fastcompras.com.br>
2019-09-09 18:09:54 -0300

    Realizando testes na inserção vue na view.

---
+++ view/vue/Acoes.vue
@@ -0,0 +1,43 @@
+let x = new Vue({
+			el: '#app2',
+			data: {
+				message:'Botão acionado',
+				counter:0
+			},
+			template: '<div id="example-1"><button v-on:mouseleave="counter += 1">Adiciona 1</button><p>{{message}}: {{ counter }}</p></div>'
+		});
+
+new Vue({
+	el: '#app',
+	data: {
+		options: [
+			{
+				id: 0,
+				label: 'Pedra'
+
+			},
+			{
+				id: 1,
+				label: 'Papel'
+			},
+			{
+				id: 2,
+				label: 'Tesoura'
+			},
+			{
+				id: 3,
+				label: 'Largarto'
+			},
+			{
+				id: 4 ,
+				label: 'Spock'
+			}
+		]
+	},
+	methods: {
+		makeMove: function(evt){
+			console.log(evt);
+		}
+	},
+	template: '<ul><li v-for="option in options"><button v-on:click="makeMove(option.id)">{{option.label}}</button></li></ul>'
+});
