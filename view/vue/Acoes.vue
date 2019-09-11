2d06affa7aa678f57f5a785d9b96bbaeffa25a95
guilhermear <guilhermear@fastcompras.com.br>
2019-09-11 13:26:26 -0300

    criando elementos dinamicos com vue.

--- view/vue/Acoes.vue
+++ view/vue/Acoes.vue
@@ -1,15 +1,23 @@
-let x = new Vue({
-			el: '#app2',
-			data: {
-				message:'Botão acionado',
-				counter:0
-			},
-			template: '<div id="example-1"><button v-on:mouseleave="counter += 1">Adiciona 1</button><p>{{message}}: {{ counter }}</p></div>'
-		});
-
-new Vue({
-	el: '#app',
+var started = new Vue({
+	el: '#nomeUser',
 	data: {
+		userName: ''
+	},
+	methods: {
+		insertName: function() {
+			if(this.userName === ''){
+				this.userName = 'João Silva'+ Math.floor(Math.random() * 100);
+			}
+			document.querySelector('.choiceUserName').style.display = "none";
+			document.querySelector('.content').style.display = "block";
+		}
+	},
+	template: '<div id="nomeUser"><h3>Digite seu nome para começar:</h3><input v-model="userName"><button v-on:click="insertName">Salvar Nome</button></div>'
+});
+
+var game = new Vue({
+	el: '#ambiente',
+	data: {	
 		options: [
 			{
 				id: 0,
@@ -36,8 +44,17 @@ new Vue({
 	},
 	methods: {
 		makeMove: function(evt){
-			makeChoice(evt);
+			makeChoice(evt); //Enviando a opção escolhida pelo usuário
 		}
 	},
-	template: '<ul><li v-for="option in options"><button v-on:click="makeMove(option.id)">{{option.label}}</button></li></ul>'
+	template: '<div><ul><li v-for="option in options"><button v-on:click="makeMove(option.id)">{{option.label}}</button></li></ul></div>'
+});
+
+var server = new Vue({
+	el: "#serverOut",
+	data: {
+		serverOut: ''
+	},
+	template: '<p>{{serverOut}}</p>'
+
 });
